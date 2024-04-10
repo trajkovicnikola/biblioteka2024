61cf85f6fec4bbc0b6719c372cecaf245da16880
Nikola Trajkovic <trajkovic.nikola2001@gmail.com>
2024-04-10 23:43:41 +0200

    ovo je novi commit na main grani

--- BibliotekaMoja/src/biblioteka/Knjiga.java
+++ BibliotekaMoja/src/biblioteka/Knjiga.java
@@ -1,12 +1,37 @@
 package biblioteka;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Knjiga {
-		
 
 	private String naslov;
 	private Long isbn;
+
+	@Override
+	public String toString() {
+		return "Knjiga [naslov=" + naslov + ", isbn=" + isbn + ", autori=" + autori + ", izdavac=" + izdavac
+				+ ", izdanje=" + izdanje + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(autori, isbn, izdanje, izdavac, naslov);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		return Objects.equals(autori, other.autori) && Objects.equals(isbn, other.isbn) && izdanje == other.izdanje
+				&& Objects.equals(izdavac, other.izdavac) && Objects.equals(naslov, other.naslov);
+	}
+
 	private List<Autor> autori;
 	private String izdavac;
 	private int izdanje;
@@ -26,8 +51,6 @@ public class Knjiga {
 	public void setIsbn(Long isbn) {
 		this.isbn = isbn;
 	}
-
-	
 
 	public List<Autor> getAutori() {
 		return autori;
@@ -52,7 +75,5 @@ public class Knjiga {
 	public void setIzdanje(int izdanje) {
 		this.izdanje = izdanje;
 	}
-	
+
 }
-
-
